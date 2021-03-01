78295743ac85f8a718e51388132dd1623795e89f
hillaryomondi <Hillary.7166@>
2021-03-02 00:40:02 +0300

    Added Registration functions on the RegistrationActivity

--- app/src/main/java/com/example/expensetrackerapp/RegistrationActivity.java
+++ app/src/main/java/com/example/expensetrackerapp/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.example.expensetrackerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -17,10 +25,19 @@ public class RegistrationActivity extends AppCompatActivity {
     private Button btnReg;
     private TextView mSignin;
 
+    private ProgressDialog mDialog;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mDialog=new ProgressDialog(this);
 
         registration();
     }
@@ -32,33 +49,60 @@ public class RegistrationActivity extends AppCompatActivity {
         mSignin=findViewById(R.id.signin_here);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                                      @Override
+                                      public void onClick(View v) {
 
-                String email=mEmail.getText().toString().trim();
-                String pass=mPass.getText().toString().trim();
+                                          String email = mEmail.getText().toString().trim();
+                                          String pass = mPass.getText().toString().trim();
 
-                //If user does not input the email address
-                if (TextUtils.isEmpty(email)){
-                    mEmail.setError("Email Required..");
-                    return;
-                }
+                                          //If user does not input the email address
+                                          if (TextUtils.isEmpty(email)) {
+                                              mEmail.setError("Email Required..");
+                                              return;
+                                          }
 
-                //if user does not input the password
-                if (TextUtils.isEmpty(pass)){
-                    mPass.setError("Password is Required.");
-                }
+                                          //if user does not input the password
+                                          if (TextUtils.isEmpty(pass)) {
+                                              mPass.setError("Password is Required.");
+                                          }
+
+                                          mDialog.setMessage("Processing");
+                                          mDialog.show();
+
+                                          mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                              @Override
+                                              public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                                  if (task.isSuccessful()) {
+
+                                                      mDialog.dismiss();
+
+                                                      Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+
+                                                      startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+                                                  } else {
+
+                                                      mDialog.dismiss();
+
+                                                      Toast.makeText(getApplicationContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
+
+                                                  }
 
 
+                                              }
+                                          });
+
+                                      }
+                                  });
+
+
+                //Signin activity
+                mSignin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
             }
-        });
-
-        //Signin activity
-        mSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        });
-    }
-}
+        }
