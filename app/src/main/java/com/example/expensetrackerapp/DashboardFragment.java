68848f625b5584832c34499821679c34b636bb53
hillaryomondi <Hillary.7166@>
2021-03-05 01:16:32 +0300

    Added Floating Action Button Design

--- app/src/main/java/com/example/expensetrackerapp/DashboardFragment.java
+++ app/src/main/java/com/example/expensetrackerapp/DashboardFragment.java
@@ -7,58 +7,86 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //Floating Button
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //Floating button textview
+    private TextView fab_income_txt;
+    private TextView fab_expense_txt;
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View myview = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        //Connect floating button to layout
+        fab_main_btn = myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn = myview.findViewById(R.id.income_ft_button);
+        fab_expense_btn = myview.findViewById(R.id.expense_ft_btn);
+
+        //connect floating text
+        fab_income_txt = myview.findViewById(R.id.income_ft_text);
+        fab_expense_txt = myview.findViewById(R.id.expense_ft_text);
+
+        //Animation connect
+        Animation FadOpen;
+        FadOpen = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
+        Animation FadeClose;
+        FadeClose = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_close);
+
+        //Boolean
+        final boolean[] isOpen = {false};
+
+        //Animation
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOpen[0]) {
+
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_expense_btn.startAnimation(FadeClose);
+                    fab_income_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+
+                    fab_income_txt.startAnimation(FadeClose);
+                    fab_expense_txt.startAnimation(FadeClose);
+                    fab_income_txt.setClickable(false);
+                    fab_expense_txt.setClickable(false);
+                    isOpen[0] = false;
+
+                } else {
+
+                    fab_income_btn.startAnimation(FadOpen);
+                    fab_expense_btn.startAnimation(FadOpen);
+                    fab_income_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+
+                    fab_income_txt.startAnimation(FadOpen);
+                    fab_expense_txt.startAnimation(FadOpen);
+                    fab_income_txt.setClickable(true);
+                    fab_expense_txt.setClickable(true);
+                    isOpen[0] = true;
+
+
+                }
+            }
+        });
+
+        return myview;
     }
 }
